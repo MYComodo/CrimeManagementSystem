25f94f1b961c48e523002e90b5e615eee2a19d58
MYComodo <my956024@gmail.com>
2020-01-29 23:53:45 +0300

    FirstCommit

---
+++ WA.CRM/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WA.CRM.db;
+using WA.CRM.Models;
+
+namespace WA.CRM.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCTestEntities entities = new MVCTestEntities();
+        public ActionResult Index()
+        {
+
+
+
+            return View(entities.MainAdmin);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WA.CRM/Models/AdminListModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WA.CRM.db;
+
+namespace WA.CRM.Models
+{
+    public class AdminListModel
+    {
+        private MVCTestEntities entities = new MVCTestEntities();
+
+        List<AdminModel> admins = new List<AdminModel>();
+    }
+}
